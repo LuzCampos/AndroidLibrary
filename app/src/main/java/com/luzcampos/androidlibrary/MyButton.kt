6c790309d219f80fa5b5e2ac4aa57025fe3b5033
LuzCampos <luzgenesis.7@gmail.com>
2024-03-01 11:53:01 -0500

    Initial commit

---
+++ app/src/main/java/com/luzcampos/androidlibrary/MyButton.kt
@@ -0,0 +1,24 @@
+package com.luzcampos.androidlibrary
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun MyButton(onClick : () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Button(onClick = onClick) {
+            Text(text = "My First Button Library")
+        }
+
+    }
+}
